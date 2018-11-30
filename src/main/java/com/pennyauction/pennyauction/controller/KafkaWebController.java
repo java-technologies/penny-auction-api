7cac45830812075ec8f74fc586e2e7faca6afe7c
dimabranik <dmitriybranitskiy@gmail.com>
2018-11-30 17:24:35 +0200

    Added controller to demonstrate working with kafka

---
+++ src/main/java/com/pennyauction/pennyauction/controller/KafkaWebController.java
@@ -0,0 +1,23 @@
+package com.pennyauction.pennyauction.controller;
+
+import com.pennyauction.pennyauction.kafka.KafkaSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
+public class KafkaWebController {
+
+    @Autowired
+    KafkaSender kafkaSender;
+
+    @PostMapping("/kafka/{topicName}")
+    public String sendToTopic(@PathVariable String topicName, @RequestBody String message) {
+        System.out.println("try to send message to " + topicName);
+        kafkaSender.send(topicName, message);
+        return "Message sent";
+    }
+
+}
